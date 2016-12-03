7790313bcc136bb9ba63636562b0a333a1402726
dlhansford <davhans@microsoft.com>
2016-12-03 14:11:41 -0600

    more jquery

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
 $('h1').on('click', function(){
     alert('You clicked on the h1. You must feel proud of yourself.');
 });
+
+$("input").keypress(function(){
+    alert('You press the magic button!');
+});
+
+$("li").on('click', function(){
+    $(this).remove();
+});
